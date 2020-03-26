e41d7a4b345348dc23fb0910422b25e1f22f8487
redaxee <47358833+redaxee@users.noreply.github.com>
2020-03-27 02:13:06 +0700

    Update eg.js

--- eg.js
+++ eg.js
@@ -143,12 +143,12 @@ class Client {
 
     loadGUI() {
         $('.agario-promo-container').replaceWith(`
-        <input onchange="localStorage.setItem('botNick', this.value);" id="botNick" maxlength="15" class="form-control" placeholder="Bot Name" value="Bot"></input>
-        <input onchange="localStorage.setItem('botAmount', this.value);" id="BotAmount" maxlength="3" class="form-control" placeholder="Bot Amount" value="10"></input>
+        <input onchange="localStorage.setItem('botNick', this.value);" id="botNick" maxlength="15" class="form-control" placeholder="Bot Name" value="BOT-ExhaleGlory"></input>
+        <input onchange="localStorage.setItem('botAmount', this.value);" id="BotAmount" maxlength="3" class="form-control" placeholder="Bot Amount" value="999"></input>
         <center><button id="toggleButton" onclick="window.client.startBots(localStorage.getItem('botAmount'));" class="btn btn-success">Start Bots</button></center>
         `);
         if (!localStorage.getItem('botAmount')) localStorage.setItem('botAmount', 10);
-        if (!localStorage.getItem('botNick')) localStorage.setItem('botNick', 'Sanik');
+        if (!localStorage.getItem('botNick')) localStorage.setItem('botNick', 'BOT-ExhaleGlory');
         console.log('[ExhaleGlory] Ready!');
     }
 
